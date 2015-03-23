ed1581d00f60d181fdf3099322a52c7d5fec86c3
Noah Azarin <noah@noah-thinkpad-x200.(none)>
2015-03-22 21:08:25 -0700

    first commit

---
+++ tests/finance_notifier_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+from finance_notifier import finance_notifier
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
